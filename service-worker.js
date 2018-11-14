158d3f77c17eb0d7588ad211c2caeb10dcc53b62
ooeygooeygold <amichaelj1@gmail.com>
2018-11-14 17:05:15 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio-master/precache-manifest.b1415dbb3dd056073e6c20144d6eb6d5.js"
+  "/portfolio-master/precache-manifest.8fc0636353fd4a2f7738dfb63e5249c8.js"
 );
 
 workbox.clientsClaim();
