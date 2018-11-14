b22c081c7a6e47251506ea97a10c65945060aff3
ooeygooeygold <amichaelj1@gmail.com>
2018-11-14 15:17:17 -0500

    Updates

---
+++ precache-manifest.3db7bade0976de6afda33efdd8f38da1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7594254f8432f8349b40",
+    "url": "/portfolio-master/static/js/runtime~main.7594254f.js"
+  },
+  {
+    "revision": "0742f514da317823c7be",
+    "url": "/portfolio-master/static/js/main.0742f514.chunk.js"
+  },
+  {
+    "revision": "e04b72f7baa6b949db21",
+    "url": "/portfolio-master/static/js/1.e04b72f7.chunk.js"
+  },
+  {
+    "revision": "0742f514da317823c7be",
+    "url": "/portfolio-master/static/css/main.97b1491f.chunk.css"
+  },
+  {
+    "revision": "642c8b64b3e0709467067d9004b76e72",
+    "url": "/portfolio-master/index.html"
+  }
+];
